e8c7b55a7ebeb028919ee9820257e2fd608ba1de
Hrexandro <marcin_nowak_hrx@o2.pl>
2026-03-29 00:08:14 +0100

    coś się nie zapisało

--- src/main.jsx
+++ src/main.jsx
@@ -10,3 +10,16 @@ createRoot(document.getElementById('root')).render(
     <App />
   </BrowserRouter>,
 )
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
